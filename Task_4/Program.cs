0f93be935992a9918f93477247a3e5266ddc16f5
50_Satoshi <celovekzivoj18@gmail.com>
2022-08-21 11:50:40 +0300

    Task_4_threeNum

---
+++ Task_4/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три 
+// числа и выдаёт максимальное из этих чисел.
+
+    {
+            int minNum=0, maxNum=0;
+            Console.WriteLine("Введите размерность массива:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] mas = new int[n];
+            Console.WriteLine("Введите массив:");
+
+            for(int i = 0;i<n;i++)
+            {
+                Console.WriteLine("Введите {0}-й элемент", i + 1);
+                mas[i] = int.Parse(Console.ReadLine());
+            }
+
+            var str = string.Join(" ", mas);
+            Console.WriteLine(str);
+            
+            maxNum=mas[0];
+            minNum=mas[0];
+            for(int i = 0;i<n;i++)
+            {
+                if(mas[i]>maxNum)
+                {
+                    maxNum=mas[i];
+                }
+                
+                if(mas[i]<minNum) 
+                {
+                    minNum=mas[i];
+                }   
+            } 
+
+            Console.WriteLine("Максимальное число {0}",maxNum);
+            Console.WriteLine("Минимальное число {0}",minNum);
+    }
+
+
+
+
+
